2ca0874fe3b6b10a923d79abe04eb065e59e42d7
Fernando Gonzalez <feragon42@gmail.com>
2016-08-09 20:36:40 -0400

    Last changes. Preparing the folder for the new (and, plz god, the definitely) version.

--- public/scripts/typing.js
+++ public/scripts/typing.js
@@ -1,15 +1,12 @@
 $(function(){
   $(".selfTyping").typed({
     //Por si quiero editar esto luego: https://github.com/mattboldt/typed.js/
-    strings: ["Programador.", 
-      "Escritor^1000... o intentando serlo.",
-      "Ingeniero en Informacion^1000... muy pronto.", // ^1000: Escribir lo siguiente en 1 seg
-      "Gamer empedernido.",
-      "Geek sin remedio.",
-      "Game Designer^1000... aspirante.",
-      "Lector apacionado.",
-      "Y, lo mas importante^1000 Web Developer.",
-      "Habil en FrontEnd^1000... e intentandolo ser con BackEnd."],
+    strings: [ 
+      "Information Engineer", // ^1000: Escribir lo siguiente en 1 seg
+      "FullStack Developer.",
+      "Workaholic.",
+      "In love with 5 of the 10 arts."
+    ],
     typeSpeed: 0,
     backDelay: 1500,
     loop: true,
